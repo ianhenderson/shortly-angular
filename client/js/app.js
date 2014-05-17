79a681fcec75c5c17ce5d26255322d0fbb067abe
Hack Reactor Students <ianwhen@gmail.com>
2014-05-16 19:44:21 -0700

    Implemented basic AJAX functionality

--- client/js/app.js
+++ client/js/app.js
@@ -1,1 +1,35 @@
+var app = angular.module('Shortly', [])
+  .controller('linksController', function($scope, $http){
+    $http({method: 'GET', url: 'http://localhost:4568/links'}).
+      success(function(data, status, headers, config) {
+        $scope.links = data;
+        console.log(data);
+      }).
+      error(function(data, status, headers, config) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+      });
+  })
 
+  .controller('shortenController', function($scope, $http){
+
+    $scope.submit = function(){
+      $scope.message = {
+        url: $scope.text
+      };
+
+      $http({
+        method: 'POST',
+        url: 'http://localhost:4568/links',
+        data: JSON.stringify($scope.message),
+      }).
+        success(function(data, status, headers, config) {
+          $scope.links = data;
+          console.log(data);
+        }).
+        error(function(data, status, headers, config) {
+          // called asynchronously if an error occurs
+          // or server returns response with an error status.
+        });
+    };
+  });
